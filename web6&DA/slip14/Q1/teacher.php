b53f5eba1e9a05f857423fa9b2a8933acf7f1765
Gaurav Rajput <137691243+GauravRajput123@users.noreply.github.com>
2024-04-11 23:13:38 +0530

    Add files via upload

---
+++ web6&DA/slip14/Q1/teacher.php
@@ -0,0 +1,30 @@
+<?php 
+/*
+CREATE TABLE IF NOT EXISTS public.teacher
+(
+    id bigint,
+    name text COLLATE pg_catalog."default",
+    branch text COLLATE pg_catalog."default",
+    exp integer
+)
+*/
+$id = $_GET['id'];
+
+$conn = pg_connect("host=localhost user=postgres password=tybcs dbname=sample");
+if($conn!=null)
+{
+    $query = "select * from public.teacher where id = '$id'";
+    $res = pg_query($conn , $query);
+    while($row = pg_fetch_array($res))
+    {
+        echo "Teacher id : $row[0] <br>";
+        echo "Teacher Name : $row[1] <br>";
+        echo "Teacher qualification : $row[2] <br>";
+        echo "Teacher salary : $row[3] <br>";
+
+    }
+}
+else{
+    echo "Connection is not enstablished";
+}
+
